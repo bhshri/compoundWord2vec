ad9ffc9f2cef810041c83e39e0dcf0ef95c7a00f
Shripad Bhat <shripadbhat30@gmail.com>
2020-10-10 21:55:27 +0530

    compoundword2vec script

--- demo-word.sh
+++ demo-word.sh
@@ -1,7 +1,2 @@
 make
-if [ ! -e text8 ]; then
-  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
-  gzip -d text8.gz -f
-fi
-time ./word2vec -train text8 -output vectors.bin -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
-./distance vectors.bin
+time ./word2vec -train 'text_corpus.txt' -output vectors.txt -cbow 0 -window 10 -negative 5 -threads 8 -binary 0 -iter 5 -save-vocab vocab.txt -constituent_compound_file 'constituent_compound_filtered.txt' 
