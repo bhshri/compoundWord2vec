c43b90417be5e7992291e99f97ff991d5e8d8c30
Shripad Bhat <shripadbhat30@gmail.com>
2020-11-22 17:35:15 +0530

    Update demo-word.sh

--- demo-word.sh
+++ demo-word.sh
@@ -1,2 +1,2 @@
 make
-time ./word2vec -train 'text_corpus.txt' -output vectors.txt -cbow 0 -window 10 -negative 5 -threads 8 -binary 0 -iter 5 -save-vocab vocab.txt -constituent_compound_file 'constituent_compound_filtered.txt' 
+time ./word2vec -train 'text_corpus.txt' -output Cword2vec_vectors.txt -cbow 0 -window 10 -negative 5 -threads 16 -binary 0 -iter 5 -save-vocab vocab.txt -constituent_compound_file 'cword2vec_constituent_compound_mapping.txt' -constituent_compound_replace_prob 1.0
